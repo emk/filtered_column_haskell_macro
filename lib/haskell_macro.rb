eaa8c5a6c21b50336cb0ac7a76145810a30fc0ca
Eric Kidd <git@randomhacks.net>
2008-12-09 08:37:36 -0500

    Update CSS and eliminate cross-site scripting attacks
    The original plugin code allowed people to inject script tags into the
    generated HTML.  As a safety precaution, fix all these problems and add
    unit tests to make sure they're fixed.
    
    We also update our CSS classes to be a little less Typo-specific.

--- lib/haskell_macro.rb
+++ lib/haskell_macro.rb
@@ -3,34 +3,42 @@ class HaskellMacro < FilteredColumn::Macros::Base
   def self.filter(attributes, inner_text='', text='')
     lang       = attributes['lang'] || 'haskell'
     title      = attributes['title']
-    cssclass   = attributes['class']
+    cssclass   = attributes['class'] || ''
     linenumber = attributes['linenumber']
 
     inner_text = inner_text.to_s.gsub(/\r/,'').gsub(/\A\n/,'').chomp
 
-    IO.popen("HsColour -css", "r+") do |f|
-      pid = fork { f.write inner_text; f.close; Kernel.exit!(0) }
-      f.close_write
-      text = f.read
-      Process.waitpid pid
-    end
+    html =
+      IO.popen("HsColour -css", "r+") do |f|
+        pid = fork { f.write inner_text; f.close; Kernel.exit!(0) }
+        f.close_write
+        result = f.read
+        Process.waitpid pid
+        result
+      end
     
-    text.gsub!(/.*<pre>/m,"<pre><code class=\"typocode_#{lang} #{cssclass}\">")
-    text.gsub!(/<\/pre>.*/m,"</code></pre>")
+    html.gsub!(/.*<pre>/m,"<pre><code class=\"#{h lang}_code #{h cssclass}\">")
+    html.gsub!(/<\/pre>.*/m,"</code></pre>")
 
-    if(linenumber)
-      lines = text.split(/\n/).size
-      linenumbers = (1..lines).to_a.collect{|line| line.to_s}.join("\n")
+    if linenumber
+      lines = html.split(/\n/).size
+      linenumbers_html = (1..lines).to_a.collect{|line| line.to_s}.join("\n")
 
-      text = "<table class=\"typocode_linenumber\"><tr><td class=\"lineno\">\n<pre>\n#{linenumbers}\n</pre>\n</td><td width=\"100%\">#{text}</td></tr></table>"
+      html = "<table><tr><td class=\"line_numbers\">\n<pre>\n#{linenumbers_html}\n</pre>\n</td><td class=\"code\" width=\"100%\">#{html}</td></tr></table>"
     end
 
-    if(title)
-      titlecode="<div class=\"codetitle\">#{title}</div>"
+    if title
+      title_html = "<div class=\"code_title\">#{h title}</div>"
     else
-      titlecode=''
+      title_html = ''
     end
 
-    "<div class=\"typocode\">#{titlecode}#{text}</div>"
+    "<div class=\"code_block\">#{title_html}#{html}</div>"
+  end
+
+  protected
+
+  def self.h str
+    CGI.escape(str)
   end
 end
--- test/haskell_macro_test.rb
+++ test/haskell_macro_test.rb
@@ -8,17 +8,29 @@ class HaskellMacroTest < ActiveSupport::TestCase
 
   test "should syntax-highlight Haskell code" do
     html = process_macros "<macro:haskell>type M a = M a</macro:haskell>"
-    expected = "<div class=\"typocode\"><pre><code class=\"typocode_haskell \"><span class='keyword'>type</span> <span class='conid'>M</span> <span class='varid'>a</span> <span class='keyglyph'>=</span> <span class='conid'>M</span> <span class='varid'>a</span></code></pre></div>"
+    expected = "<div class=\"code_block\"><pre><code class=\"haskell_code \"><span class='keyword'>type</span> <span class='conid'>M</span> <span class='varid'>a</span> <span class='keyglyph'>=</span> <span class='conid'>M</span> <span class='varid'>a</span></code></pre></div>"
     assert_equal expected, html
   end
 
   test "should remove leading and trailing newlines" do
     html = process_macros "<macro:haskell>\ntype M a = M a\n</macro:haskell>"
-    expected = "<div class=\"typocode\"><pre><code class=\"typocode_haskell \"><span class='keyword'>type</span> <span class='conid'>M</span> <span class='varid'>a</span> <span class='keyglyph'>=</span> <span class='conid'>M</span> <span class='varid'>a</span></code></pre></div>"
+    expected = "<div class=\"code_block\"><pre><code class=\"haskell_code \"><span class='keyword'>type</span> <span class='conid'>M</span> <span class='varid'>a</span> <span class='keyglyph'>=</span> <span class='conid'>M</span> <span class='varid'>a</span></code></pre></div>"
     assert_equal expected, html
   end
 
+  test "should not allow XSS attacks" do
+    assert_no_xss "<macro:haskell lang=\"<script>\">x=1</macro:haskell>"
+    assert_no_xss "<macro:haskell title=\"<script>\">x=1</macro:haskell>"
+    assert_no_xss "<macro:haskell class=\"<script>\">x=1</macro:haskell>"
+    assert_no_xss "<macro:haskell><script></macro:haskell>"
+  end
+
   private
+
+  def assert_no_xss macro
+    html = process_macros macro
+    assert_no_match /<script/, html
+  end
 
   def process_macros(text)
     FilteredColumn::Processor.new(nil, text).filter
